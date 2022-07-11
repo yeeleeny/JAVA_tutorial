136430072f23af57fb9a33e6ea376925f2987af3
Yerin Song <angelrin7@naver.com>
2022-07-11 17:26:36 +0900

    initial commit

--- src/java_project/HelloWorld.java
+++ src/java_project/HelloWorld.java
@@ -1,7 +1,10 @@
 package java_project;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
 	}
+
 }
